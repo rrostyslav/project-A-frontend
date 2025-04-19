3a5d60f1449c64d0ac4decf59e334ce95ed887c6
balanced <vladyslav.herasymenko@infotech.gov.ua>
2025-04-19 23:51:24 +0300

    feat: update styles and add project configuration

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite';
-import solid from 'vite-plugin-solid';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { defineConfig } from 'vite'
+import solid from 'vite-plugin-solid'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [TanStackRouterVite({ target: 'solid' }), solid()],
-});
+})
