4060d850130864ac5099e4721d9be2ffc8a01d4a
Rostyslav Rachynskyi <r.rostik2000@gmail.com>
2025-04-20 07:11:04 +0300

    feat: setup unit tests - vitest

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,36 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/solid-query'
-import { createRouter, RouterProvider } from '@tanstack/solid-router'
-import { render } from 'solid-js/web'
-import { routeTree } from './routeTree.gen'
-import './styles.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/solid-query";
+import { createRouter, RouterProvider } from "@tanstack/solid-router";
+import { render } from "solid-js/web";
+import { routeTree } from "./routeTree.gen";
+import "./styles.css";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 // Set up a Router instance
 const router = createRouter({
-  routeTree,
-  defaultPreload: 'intent',
-  defaultStaleTime: 5000,
-  scrollRestoration: true,
-})
+	routeTree,
+	defaultPreload: "intent",
+	defaultStaleTime: 5000,
+	scrollRestoration: true,
+});
 
 // Register things for typesafety
-declare module '@tanstack/solid-router' {
-  interface Register {
-    router: typeof router
-  }
+declare module "@tanstack/solid-router" {
+	interface Register {
+		router: typeof router;
+	}
 }
 
 const App = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  )
-}
+	return (
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	);
+};
 
-const rootElement = document.getElementById('app') as HTMLElement
+const rootElement = document.getElementById("app") as HTMLElement;
 
 if (!rootElement.innerHTML) {
-  render(() => <App />, rootElement)
+	render(() => <App />, rootElement);
 }
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,24 +1,24 @@
-import { Link, Outlet, createRootRoute } from '@tanstack/solid-router'
-import { TanStackRouterDevtools } from '@tanstack/solid-router-devtools'
+import { Link, Outlet, createRootRoute } from "@tanstack/solid-router";
+import { TanStackRouterDevtools } from "@tanstack/solid-router-devtools";
 
 export const Route = createRootRoute({
-  component: RootComponent,
-  notFoundComponent: () => {
-    return (
-      <div>
-        <p>This is the notFoundComponent configured on root route</p>
-        <Link to="/">Start Over</Link>
-      </div>
-    )
-  },
-})
+	component: RootComponent,
+	notFoundComponent: () => {
+		return (
+			<div>
+				<p>This is the notFoundComponent configured on root route</p>
+				<Link to="/">Start Over</Link>
+			</div>
+		);
+	},
+});
 
 function RootComponent() {
-  return (
-    <>
-      <Outlet />
-      {/* Start rendering router matches */}
-      <TanStackRouterDevtools position="bottom-right" />
-    </>
-  )
+	return (
+		<>
+			<Outlet />
+			{/* Start rendering router matches */}
+			<TanStackRouterDevtools position="bottom-right" />
+		</>
+	);
 }
