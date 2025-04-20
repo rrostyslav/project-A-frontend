4e54090c7fae5e566ea47601c58e1f0a76144ea1
Rostyslav Rachynskyi <r.rostik2000@gmail.com>
2025-04-20 07:11:04 +0300

    feat: setup unit tests - vitest

---
+++ src/components/Counter.tsx
@@ -0,0 +1,11 @@
+import { createSignal } from "solid-js";
+
+export const Counter = () => {
+	const [count, setCount] = createSignal(1);
+
+	return (
+		<button type="button" onClick={() => setCount(count() + 1)}>
+			{count()}
+		</button>
+	);
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,45 +1,38 @@
-<<<<<<< HEAD
-import { useQuery } from '@tanstack/solid-query'
-import { createFileRoute } from '@tanstack/solid-router'
-import { Match, Switch } from 'solid-js'
+import { useQuery } from "@tanstack/solid-query";
+import { createFileRoute } from "@tanstack/solid-router";
+import { Match, Switch } from "solid-js";
 
-export const Route = createFileRoute('/')({
-  component: Home,
-})
+export const Route = createFileRoute("/")({
+	component: Home,
+});
 
 function Home() {
-  const query = useQuery(() => ({
-    queryKey: ['repoData'],
-    queryFn: () =>
-      fetch('https://api.github.com/repos/tannerlinsley/react-query').then((res) => res.json()),
-  }))
+	const query = useQuery(() => ({
+		queryKey: ["repoData"],
+		queryFn: () =>
+			fetch("https://api.github.com/repos/tannerlinsley/react-query").then(
+				(res) => res.json(),
+			),
+	}));
 
-  return (
-    <div class="p-2 flex flex-col gap-y-4 items-center">
-      <h3 class="text-green-600 text-2xl">Starter SolidJS + Tanstack Router + Tanstack Query</h3>
+	return (
+		<div class="p-2 flex flex-col gap-y-4 items-center">
+			<h3 class="text-green-600 text-2xl">
+				Starter SolidJS + Tanstack Router + Tanstack Query
+			</h3>
 
-      <Switch>
-        <Match when={query.isPending}>Loading...</Match>
-        <Match when={query.isError}>Error: {query.error?.message}</Match>
-        <Match when={query.isSuccess}>
-          <div>
-            <p>{query.data.description}</p>
-            <strong>👀 {query.data.subscribers_count}</strong>{' '}
-            <strong>✨ {query.data.stargazers_count}</strong>{' '}
-            <strong>🍴 {query.data.forks_count}</strong>
-          </div>
-        </Match>
-      </Switch>
-    </div>
-  )
-=======
-import { createFileRoute } from '@tanstack/solid-router'
-
-export const Route = createFileRoute('/')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/"!</div>
->>>>>>> d6ecf43 (feat: biome base setup)
+			<Switch>
+				<Match when={query.isPending}>Loading...</Match>
+				<Match when={query.isError}>Error: {query.error?.message}</Match>
+				<Match when={query.isSuccess}>
+					<div>
+						<p>{query.data.description}</p>
+						<strong>👀 {query.data.subscribers_count}</strong>{" "}
+						<strong>✨ {query.data.stargazers_count}</strong>{" "}
+						<strong>🍴 {query.data.forks_count}</strong>
+					</div>
+				</Match>
+			</Switch>
+		</div>
+	);
 }
