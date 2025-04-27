33cb9baaaec56506a18fe7393870b4fec51f3b39
balanced <vladyslav.herasymenko@infotech.gov.ua>
2025-04-27 20:05:25 +0300

    feat: add SpriteIcon component and multiple SVG icons

--- src/pages/greeting/GreetingPage.vue
+++ src/pages/greeting/GreetingPage.vue
@@ -1,4 +1,7 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { SpriteIcon } from '@/shared/ui/icon'
+</script>
 <template>
   <div>Hello</div>
+  <SpriteIcon color="red" height="244" name="home" width="244" />
 </template>
