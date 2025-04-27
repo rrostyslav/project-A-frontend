0eb9acbab02c5bd8b2c7f3db4736bf7b917229b5
balanced <vladyslav.herasymenko@infotech.gov.ua>
2025-04-27 20:05:25 +0300

    feat: add SpriteIcon component and multiple SVG icons

---
+++ src/shared/ui/icon/SpriteIcon.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+defineProps({
+  name: { type: String, required: true },
+  prefix: { type: String, default: 'icon' },
+  width: { type: [String, Number], default: '1em' },
+  height: { type: [String, Number], default: '1em' },
+  color: { type: String, default: 'currentColor' },
+  ariaLabel: { type: String, default: undefined },
+})
+</script>
+
+<template>
+  <svg
+    :aria-hidden="!ariaLabel"
+    :aria-label="ariaLabel || undefined"
+    :fill="color"
+    :height="height"
+    :width="width"
+    role="img"
+    v-bind="$attrs"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <use :href="`#${prefix}-${name}`" />
+  </svg>
+</template>
