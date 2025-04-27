c77f962948b05bc82a698f1070e67f3ebaaf2edd
Rostyslav Rachynskyi <r.rostik2000@gmail.com>
2025-04-28 01:23:01 +0300

    feat: typography component

--- src/pages/greeting/GreetingPage.vue
+++ src/pages/greeting/GreetingPage.vue
@@ -1,7 +1,9 @@
 <script lang="ts" setup>
 import { SpriteIcon } from '@/shared/ui/icon'
+import Typography from '@/shared/ui/typography/Typography.vue'
 </script>
 <template>
   <div>Hello</div>
+  <Typography typography="title3" color="chart-2" weight="medium"> Test Typography </Typography>
   <SpriteIcon color="red" height="244" name="home" width="244" />
 </template>
