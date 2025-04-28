3ec024b4b16f081eb153f68a2b48255515540c57
Rostyslav Rachynskyi <r.rostik2000@gmail.com>
2025-04-28 04:08:35 +0300

    chore: revert ci to working state at 9fa5b35

--- src/pages/greeting/GreetingPage.vue
+++ src/pages/greeting/GreetingPage.vue
@@ -3,7 +3,7 @@ import { SpriteIcon } from '@/shared/ui/icon'
 import Typography from '@/shared/ui/typography/Typography.vue'
 </script>
 <template>
-  <div>Hello User!</div>
-  <Typography typography="title3" color="chart-2" weight="medium"> Test Typography test</Typography>
+  <div>Hello</div>
+  <Typography typography="title3" color="chart-2" weight="medium"> Test Typography </Typography>
   <SpriteIcon color="red" height="244" name="home" width="244" />
 </template>
