d4ffbed882c123550dcba276a443376fd6cd3328
Rostyslav Rachynskyi <r.rostik2000@gmail.com>
2025-04-28 03:12:28 +0300

    feat: change greeting (test ci)

--- src/pages/greeting/GreetingPage.vue
+++ src/pages/greeting/GreetingPage.vue
@@ -3,7 +3,7 @@ import { SpriteIcon } from '@/shared/ui/icon'
 import Typography from '@/shared/ui/typography/Typography.vue'
 </script>
 <template>
-  <div>Hello</div>
+  <div>Hello User!</div>
   <Typography typography="title3" color="chart-2" weight="medium"> Test Typography </Typography>
   <SpriteIcon color="red" height="244" name="home" width="244" />
 </template>
