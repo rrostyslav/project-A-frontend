5578aff66c0a460902254141be5cce2398d54364
balanced <vladyslav.herasymenko@infotech.gov.ua>
2025-05-02 13:53:35 +0300

    refactor(Typography): component to use class-variance-authority for styling

--- src/shared/ui/typography/Typography.vue
+++ src/shared/ui/typography/Typography.vue
@@ -1,7 +1,8 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 import type { ThemeColor } from '@/shared/types/theme'
 import { Primitive } from 'reka-ui'
 import { computed } from 'vue'
+import { cva } from 'class-variance-authority'
 
 type TypographyVariant =
   | 'title1'
@@ -14,39 +15,58 @@ type TypographyVariant =
   | 'mini'
   | 'micro'
 
-const props = defineProps<{
-  typography: TypographyVariant
-  weight?: 'regular' | 'medium'
-  as?: keyof HTMLElementTagNameMap
-  color?: ThemeColor
-}>()
-
-const typographyClasses = computed(() => {
-  const sizeMap: Record<TypographyVariant, string> = {
-    title1: 'text-4xl leading-none',
-    title2: 'text-2xl leading-none',
-    'issue-title': 'text-xl leading-none',
-    title3: 'text-lg leading-none',
-    large: 'text-base leading-7',
-    normal: 'text-sm leading-6',
-    small: 'text-[13px] leading-none',
-    mini: 'text-xs leading-none',
-    micro: 'text-[11px] leading-[110%]',
-  }
-
-  const fontWeight = props.weight === 'medium' ? 'font-medium' : 'font-normal'
-  const baseClasses = sizeMap[props.typography] || ''
-
-  return `${fontWeight} ${baseClasses} text-[color:var(--color)]`
+const typographyStyles = cva('text-[color:var(--color)]', {
+  variants: {
+    typography: {
+      title1: 'text-4xl leading-none',
+      title2: 'text-2xl leading-none',
+      'issue-title': 'text-xl leading-none',
+      title3: 'text-lg leading-none',
+      large: 'text-base leading-7',
+      normal: 'text-sm leading-6',
+      small: 'text-[13px] leading-none',
+      mini: 'text-xs leading-none',
+      micro: 'text-[11px] leading-[110%]',
+    },
+    weight: {
+      regular: 'font-normal',
+      medium: 'font-medium',
+    },
+  },
+  defaultVariants: {
+    typography: 'normal',
+    weight: 'regular',
+  },
 })
 
-const color = computed(() => ({
-  '--color': `var(--${props.color || 'primary'})`,
+const props = withDefaults(
+  defineProps<{
+    typography: TypographyVariant
+    weight?: 'regular' | 'medium'
+    as?: keyof HTMLElementTagNameMap
+    color?: ThemeColor
+  }>(),
+  {
+    weight: 'regular',
+    as: undefined,
+    color: 'primary',
+  },
+)
+
+const classes = computed(() => {
+  return typographyStyles({
+    typography: props.typography,
+    weight: props.weight,
+  })
+})
+
+const colorStyle = computed(() => ({
+  '--color': `var(--${props.color})`,
 }))
 </script>
 
 <template>
-  <Primitive :as="as || 'p'" :class="typographyClasses" :style="color">
+  <Primitive :as="as || 'p'" :class="classes" :style="colorStyle">
     <slot />
   </Primitive>
 </template>
